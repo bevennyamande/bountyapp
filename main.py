cd416332a138f1c8a20b096876bb2329fa356f5d
Beven Nyamande <bevennyamande@gmail.com>
2021-06-02 14:39:41 +0200

    formatted coreectly according to standards

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ from sklearn import neighbors
 from PIL import Image, ImageDraw
 from face_recognition.face_recognition_cli import image_files_in_folder
 from fastapi import FastAPI, File, UploadFile
-from train import * # import everything from the train directory
+from train import *  # import everything from the train directory
 
 app = FastAPI()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, 'trained_knn_model.clf')
-TEMP_DIR = os.path.join(BASE_DIR, 'tmp')
-TRAIN_DIR = os.path.join(BASE_DIR, 'train')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+MODEL_PATH = os.path.join(BASE_DIR, "trained_knn_model.clf")
+TEMP_DIR = os.path.join(BASE_DIR, "tmp")
+TRAIN_DIR = os.path.join(BASE_DIR, "train")
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
+def train(
+    train_dir,
+    model_save_path=None,
+    n_neighbors=None,
+    knn_algo="ball_tree",
+    verbose=False,
+):
     """
     Trains a k-nearest neighbors classifier for face recognition.
 
@@ -62,11 +68,21 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
             if len(face_bounding_boxes) != 1:
                 # If there are no people (or too many people) in a training image, skip the image.
                 if verbose:
-                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+                    print(
+                        "Image {} not suitable for training: {}".format(
+                            img_path,
+                            "Didn't find a face"
+                            if len(face_bounding_boxes) < 1
+                            else "Found more than one face",
+                        )
+                    )
             else:
                 # Add face encoding for current image to the training set
-                X.append(face_recognition.face_encodings(image,
-                                known_face_locations=face_bounding_boxes)[0])
+                X.append(
+                    face_recognition.face_encodings(
+                        image, known_face_locations=face_bounding_boxes
+                    )[0]
+                )
                 y.append(class_dir)
 
     # Determine how many neighbors to use for weighting in the KNN classifier
@@ -76,14 +92,14 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
             print("Chose n_neighbors automatically:", n_neighbors)
 
     # Create and train the KNN classifier
-    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors,
-                                             algorithm=knn_algo,
-                                             weights='distance')
+    knn_clf = neighbors.KNeighborsClassifier(
+        n_neighbors=n_neighbors, algorithm=knn_algo, weights="distance"
+    )
     knn_clf.fit(X, y)
 
     # Save the trained KNN classifier
     if model_save_path is not None:
-        with open(model_save_path, 'wb') as f:
+        with open(model_save_path, "wb") as f:
             pickle.dump(knn_clf, f)
 
     return knn_clf
@@ -101,15 +117,20 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
     :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
         For faces of unrecognized persons, the name 'unknown' will be returned.
     """
-    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
+    if (
+        not os.path.isfile(X_img_path)
+        or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS
+    ):
         raise Exception("Invalid image path: {}".format(X_img_path))
 
     if knn_clf is None and model_path is None:
-        raise Exception("Must supply knn classifier either through knn_clf or model_path")
+        raise Exception(
+            "Must supply knn classifier either through knn_clf or model_path"
+        )
 
     # Load a trained KNN model (if one was passed in)
     if knn_clf is None:
-        with open(model_path, 'rb') as f:
+        with open(model_path, "rb") as f:
             knn_clf = pickle.load(f)
 
     # Load image file and find face locations
@@ -121,14 +142,24 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
         return []
 
     # Find encodings for faces in the test iamge
-    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+    faces_encodings = face_recognition.face_encodings(
+        X_img, known_face_locations=X_face_locations
+    )
 
     # Use the KNN model to find the best matches for the test face
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
-    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+    are_matches = [
+        closest_distances[0][i][0] <= distance_threshold
+        for i in range(len(X_face_locations))
+    ]
 
     # Predict classes and remove classifications that aren't within the threshold
-    return [(pred, loc) if rec else ("not found", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+    return [
+        (pred, loc) if rec else ("not found", loc)
+        for pred, loc, rec in zip(
+            knn_clf.predict(faces_encodings), X_face_locations, are_matches
+        )
+    ]
 
 
 def show_prediction_labels_on_image(img_path, predictions):
@@ -152,11 +183,12 @@ def show_prediction_labels_on_image(img_path, predictions):
 
         # Draw a label with a name below the face
         text_width, text_height = draw.textsize(name)
-        draw.rectangle(((left, bottom - text_height - 10),
-                        (right, bottom)), fill=(0, 0, 255),
-                       outline=(0, 0, 255))
-        draw.text((left + 6, bottom - text_height - 5),
-                  name, fill=(255, 255, 255, 255))
+        draw.rectangle(
+            ((left, bottom - text_height - 10), (right, bottom)),
+            fill=(0, 0, 255),
+            outline=(0, 0, 255),
+        )
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
 
     # Remove the drawing library from memory as per the Pillow docs
     del draw
@@ -172,18 +204,12 @@ async def create_upload_file(image: UploadFile = File(...)):
     contents = await image.read()
     os.makedirs(TEMP_DIR, exist_ok=True)
 
-    with open(os.path.join(TEMP_DIR,image.filename), "wb") as f:
+    with open(os.path.join(TEMP_DIR, image.filename), "wb") as f:
         f.write(contents)
 
-    predictions = predict(os.path.join(TEMP_DIR,image.filename),model_path=MODEL_PATH)
+    predictions = predict(os.path.join(TEMP_DIR, image.filename), model_path=MODEL_PATH)
     try:
-        os.remove(os.path.join(TEMP_DIR,image.filename))
+        os.remove(os.path.join(TEMP_DIR, image.filename))
     except Exception as e:
         pass
-    return {"Predictions":predictions}
-
-
-
-
-
-
+    return {"Predictions": predictions}
